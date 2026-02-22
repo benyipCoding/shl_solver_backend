6a0975aaad032278ac14cbc033d73789db4fb93d
benyipCoding <benyip5207@gmail.com>
2026-02-22 17:53:17 +0800

    is_active改动

--- app/depends/jwt_guard.py
+++ app/depends/jwt_guard.py
@@ -4,10 +4,16 @@ from fastapi import HTTPException, status
 
 async def verify_user(request: Request):
     user = getattr(request.state, "user", None)
-    if not user or not user.is_active:
+    if not user:
         # 必须通过抛出异常来中断请求
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Unauthorized: User is not logged in",
+        )
+
+    if not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Forbidden: User account is inactive",
         )
     return user
