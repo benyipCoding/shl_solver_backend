137979c4171ecba3ec50950c3ff28b128a41c48c
benyipCoding <benyip5207@gmail.com>
2026-02-22 17:48:13 +0800

    user is_actionve guard

--- app/depends/jwt_guard.py
+++ app/depends/jwt_guard.py
@@ -4,7 +4,7 @@ from fastapi import HTTPException, status
 
 async def verify_user(request: Request):
     user = getattr(request.state, "user", None)
-    if not user:
+    if not user or not user.is_active:
         # 必须通过抛出异常来中断请求
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
