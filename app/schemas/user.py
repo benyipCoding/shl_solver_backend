aa34877f1a22f80b4f68ae4f01de86b711a5f611
benyipCoding <benyip5207@gmail.com>
2026-03-29 19:02:09 +0800

    update

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -5,5 +5,7 @@ class UserSerializer(BaseModel):
     id: int
     username: str
     email: EmailStr
+    is_staff: bool
+    is_superuser: bool
     # is_active: bool
     model_config = {"from_attributes": True}
